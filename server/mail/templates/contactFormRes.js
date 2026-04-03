31fc331f2776e982946693cf24800db5052724fa
shre2904 <shresthkakkar66@gmail.com>
2026-04-03 14:19:28 +0530

    fix: update broken logo URL in email templates

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -75,7 +75,7 @@ exports.contactUsEmail = (
     <body>
         <div class="container">
             <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+                    src="https://files.catbox.moe/s6nfba.png" alt="StudyNotion Logo"></a>
             <div class="message">Contact Form Confirmation</div>
             <div class="body">
                 <p>Dear ${firstname} ${lastname},</p>
